2d65afbbc7862385c3e90facf2da7c2a9d7eb23c
william arasirwa <mageboarasirwa03@gmail.com>
2023-07-01 13:34:50 +0300

    complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :leases, only: [:index, :show, :destroy, :update, :create]
+  resources :tenants, only: [:index, :show, :destroy, :update, :create]
+  resources :apartments, only: [:index, :show, :destroy, :update, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
